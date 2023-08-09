5d422ae07ef9a621d638fb7739cd52181eaad13f
Lazar Stojanović <lakisd98@gmail.com>
2023-08-09 19:53:25 +0200

    Add: pg pool setup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,18 @@
 import createApp from "./src/app.js";
+import pool from "./src/pool.js";
+import "dotenv/config";
+
 const port = 3030;
 
-createApp().listen(port, () => {
-	console.log(`Listening on http://localhost:${port}/photos`);
-});
+pool
+	.connect({
+		connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+	})
+	.then(
+		createApp().listen(port, () => {
+			console.log(`Listening on http://localhost:${port}/photos`);
+		})
+	)
+	.catch((err) => {
+		console.log(err.message);
+	});
