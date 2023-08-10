ee4adb3f369c71d28e645432a0ccfc1d12cdda41
Lazar Stojanović <lakisd98@gmail.com>
2023-08-10 16:48:05 +0200

    Add: add photo button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
+// import AddPhoto from "./components/AddPhoto";
+
 function App() {
-	return (
-		<>
-			<h1>Hello</h1>
-		</>
-	);
+	return <></>;
 }
 
 export default App;
---
+++ frontend/src/components/AddPhoto.tsx
@@ -0,0 +1,5 @@
+import styles from "./AddPhoto.module.css";
+
+export default function AddPhoto() {
+	return <button className={styles.btn}>Add a photo</button>;
+}
