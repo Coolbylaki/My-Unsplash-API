e8374c0358eca6ffd1b9caf50a7099736615de3e
Lazar Stojanović <lakisd98@gmail.com>
2023-08-11 00:17:52 +0200

    Update: api CORS header

--- src/app.js
+++ src/app.js
@@ -5,6 +5,13 @@ const createApp = () => {
 	const app = express();
 
 	app.use(express.json());
+	app.use((req, res, next) => {
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Methods", "GET,POST, DELETE");
+		res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+		next();
+	});
+
 	app.use(photoRouter);
 
 	return app;
